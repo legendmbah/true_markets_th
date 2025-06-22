9e0974ef687e68a0416694c83b02aa7cafa71c2a
bmbah <mbahbegenius@gmail.com>
2025-06-22 16:45:07 -0400

    updatted variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,13 +3,7 @@ variable "pm_api_token_secret" {
   sensitive   = true
 }
 
-variable "vm_configs" {
-  type = list(object({
-    hostname = string
-    ip       = string
-    mac      = string
-    cpu      = number
-    ram      = number
-    disk     = number
-  }))
+variable "server_count" {
+  description = "Number of Proxmox Rocky Linux VMs to deploy"
+  type        = number
 }
